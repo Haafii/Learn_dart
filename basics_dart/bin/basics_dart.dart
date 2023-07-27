c8d9314b3f0bf4848b41c823526ab74f409f1e25
Haafii <hafismuhammedtk@gmail.com>
2023-07-27 22:37:11 +0530

    number

--- basics_dart/bin/basics_dart.dart
+++ basics_dart/bin/basics_dart.dart
@@ -12,4 +12,9 @@ void main() {
   var num1 = int.parse(input1!); //change into number
   var num2 = int.parse(input2!);
   print("Sum is ${num1 + num2}");
+  //number
+  int ab = 10; //integer number
+  double ac = 10; //floating number
+  num number1 = 10; //integer
+  num number2 = 10.5; //floating number
 }
