7083bcc3232c394195bfb13f3d70de12f9eadc25
Haafii <hafismuhammedtk@gmail.com>
2023-07-29 02:13:07 +0530

    function required

--- functions/bin/functions.dart
+++ functions/bin/functions.dart
@@ -1,16 +1,19 @@
 import 'dart:io'; // Import the dart:io library
 
 void main() {
+  sum();
+  sum1(10, 12);
+  sumReq(num1: 10, num2: 20);
+}
 
+void sum() {
+  print(3 + 4);
+}
 
+void sum1(int a, int b) {
+  print('${a + b}');
+}
 
-
-
-
-
-
-
-
-
-  
+void sumReq({required num1, required int num2}) {
+  print(num1 + num2);
 }
