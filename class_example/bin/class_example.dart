a47c3b06e72070e1a65572a109ab586f3dad4c36
Haafii <hafismuhammedtk@gmail.com>
2023-07-29 21:03:22 +0530

    named constructor

--- class_example/bin/class_example.dart
+++ class_example/bin/class_example.dart
@@ -3,10 +3,21 @@ import 'package:class_example/class_example.dart' as class_example;
 class Person {
   String name = "Hafi";
   int age = 22;
+  Person(String name, int age) {
+    //constructor
+    this.name = name;
+    this.age = age;
+  }
+  Person.ageBellow50(this.name, this.age) {
+    //named constructor
+    print(name);
+  }
 }
 
 void main(List<String> arguments) {
-  final person = Person();
-  person.name = "hafii";//change name
-  print(person.name);
+  // final person = Person("Hafiiiii", 25);
+  // person.name = "hafii"; //change name
+  // print(person.name);
+  // print(person.age);
+  final person = Person.ageBellow50("Hafiiiii", 25);
 }
