c1ddbc8e29b0521a456d317f509306c3c617b0f2
Haafii <hafismuhammedtk@gmail.com>
2023-07-29 21:40:35 +0530

    exception handling

---
+++ exception_handling/bin/exception_handling.dart
@@ -0,0 +1,19 @@
+class InvalidPhoneNumberException implements Exception {}
+
+bool? validatePhoneNumber(String phone) {
+  if (phone.length == 10) {
+    return true;
+  } else {
+    throw InvalidPhoneNumberException();
+  }
+}
+
+void main(List<String> arguments) {
+  try {
+    final phoneValid = validatePhoneNumber('3433');
+  } on InvalidPhoneNumberException catch (_) {
+    print('Invalid phone number');
+  } catch (e) {
+    print(e);
+  }
+}
